15801d5386f52784e41f0c22a7f32e87d234ef0d
Jack Haynes <haynes.jack.stewart@gmail.com>
2023-05-04 03:17:19 +1200

    adds cv link, adds pingpong photo and link

--- src/Portfoio.jsx
+++ src/Portfoio.jsx
@@ -42,23 +42,33 @@ export default function Portfolio() {
             />
           </svg>
         </a>
-        <a href="https://www.google.co.nz">
+        <a
+          href="https://drive.google.com/file/d/1z6SHxXnKD8cTs-0QeaXDib2J7pBG1Yis/view?usp=share_link"
+          target="_blank"
+          rel="noreferrer"
+        >
           <img src="./cv.png" alt="cv" className="cv-img" />
         </a>
       </div>
       <div className="portfolio-div">
         <div className="project-div">
           <h4>PingPong</h4>
-          <div className="photo-div"></div>
+          <div className="photo-div">
+            <img src="./ping.png" alt="pingpong" />
+          </div>
           <p>
             This was my final group project at Dev Academy. Built in React
             Native, it is a basic social media app designed to kill your
             groupchat. Sign up, add your friends, and send a 'ping' with your
             current location to let your friends know what you're up to!
           </p>
-          <p>Built with react.js and redux.js for the front-end</p>
+          <p>Built with React Native and redux.js for the front-end</p>
           <div className="links-div">
-            <a href="https://www.google.co.nz" target="_blank" rel="noreferrer">
+            <a
+              href="https://github.com/J-Haynes/pingpong"
+              target="_blank"
+              rel="noreferrer"
+            >
               <svg
                 stroke="black"
                 fill="none"
@@ -72,26 +82,6 @@ export default function Portfolio() {
               >
                 <title>logo</title>
                 <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
-              </svg>
-            </a>
-            <a href="https://www.google.co.nz" target="_blank" rel="noreferrer">
-              <svg
-                width="40"
-                height="40"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <g id="Interface / Link">
-                  <path
-                    id="Vector"
-                    d="M9.1718 14.8288L14.8287 9.17192M7.05086 11.293L5.63664 12.7072C4.07455 14.2693 4.07409 16.8022 5.63619 18.3643C7.19829 19.9264 9.7317 19.9259 11.2938 18.3638L12.7065 16.9498M11.2929 7.05L12.7071 5.63579C14.2692 4.07369 16.8016 4.07397 18.3637 5.63607C19.9258 7.19816 19.9257 9.73085 18.3636 11.2929L16.9501 12.7071"
-                    stroke="#000000"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                </g>
               </svg>
             </a>
           </div>
