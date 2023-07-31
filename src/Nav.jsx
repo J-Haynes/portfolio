9a10df7ba7b81ff9b7dc711fb9b37a640a8375be
Jack Haynes <haynes.jack.stewart@gmail.com>
2023-08-01 02:39:34 +1200

    removes white line from navbar, adds padding to container div

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,15 +1,17 @@
 export default function NavBar() {
   return (
     <div className="navbar">
-      <div className="name-div">
-        <p>Jack Haynes Portfolio</p>
-      </div>
-      <div className="nav-list">
-        <ul>
-          <li>Home</li>
-          <li>Projects</li>
-          <li>Contact</li>
-        </ul>
+      <div className="nav-container">
+        <div className="name-div">
+          <p>Jack Haynes Portfolio</p>
+        </div>
+        <div className="nav-list">
+          <ul>
+            <li>Home</li>
+            <li>Projects</li>
+            <li>Contact</li>
+          </ul>
+        </div>
       </div>
     </div>
   )
